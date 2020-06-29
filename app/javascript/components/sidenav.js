29aa93214894cba2572326531df8daac8ab06691
Timothee Boingneres <timboing@icloud.com>
2020-06-29 12:21:03 +0200

    mobile first steps

---
+++ app/javascript/components/sidenav.js
@@ -0,0 +1,16 @@
+const initSidenav = () => {
+  const burger = document.getElementById('burger-menu');
+  if(!burger) return;
+  const sideNav = document.getElementById('tim-sidenav');
+
+  burger.addEventListener('click', (event) => {
+    if(sideNav.style.width !== "80%"){
+      sideNav.style.width = '80%';
+    }else{
+      sideNav.style.width = '0';
+    }
+  });
+
+}
+
+export{initSidenav};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@ import{initCarousel} from 'components/carousel.js'
 import { initMapbox } from '../plugins/init_mapbox';
 import { bordersAnimation } from '../fictions/show';
 import { animateBordersMainButtons } from '../components/main-buttons';
+import { initSidenav } from '../components/sidenav';
 
 animateBordersMainButtons();
 initMapbox();
 initCarousel();
 bordersAnimation();
+initSidenav();
